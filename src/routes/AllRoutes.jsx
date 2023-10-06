391656dbf9ef031aa15dfeec7f9183dc9665ee9f
vanwolf16 <d.murillo28@yahoo.com>
2023-10-06 13:06:17 -0700

    first commit

---
+++ src/routes/AllRoutes.jsx
@@ -0,0 +1,15 @@
+import { Routes,Route } from "react-router-dom";
+import { CreatePost, HomePage, PageNotFound } from "../pages";
+
+
+export const AllRoutes = () =>{
+    return(
+        <main>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="create" element={<CreatePost />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </main>
+    )
+}
